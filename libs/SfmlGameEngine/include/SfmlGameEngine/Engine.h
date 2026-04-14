0be677d08ccd2c87dfe80aa2e2c0c61c28a7bd01
TAV <tereshenkov@mail.ru>
2026-04-14 16:17:05 +0300

    docs: Комментарии в движке.

--- libs/SfmlGameEngine/include/SfmlGameEngine/Engine.h
+++ libs/SfmlGameEngine/include/SfmlGameEngine/Engine.h
@@ -15,38 +15,56 @@ private:
     unsigned int width;
     unsigned int height;
     std::string exedir;
+    // Флаги закрытия окна и закрытия текущей (верхней) сцены
     bool signal_closed;
     bool signal_exitscene;
     std::unique_ptr<sf::RenderWindow> window;
+    // Ссылки на новые сцены - корневой и верхней следующей
     std::shared_ptr<Scene> nextscene;
     std::shared_ptr<Scene> overscene;
+    // Шрифт по умолчанию
     std::shared_ptr<sf::Font> defaultfont;
+    // Текстуры для курсоров
     std::vector<std::unique_ptr<sf::Texture>> textures;
+    // Курсор по умолчанию
     std::shared_ptr<sf::Sprite> defaultcursor;
+    // Текущий курсор, установленный в методе внешнем
     std::weak_ptr<sf::Sprite> currentcursor;
+    // Карта курсоров дополнительных
     std::map<int, std::shared_ptr<sf::Sprite>> cursors;
     Texts texts;
     Colors colors;
 public:
     Engine(unsigned int width, unsigned int height) ;
+    // Основной метод - запуск движка со сценой
+    void Run(std::shared_ptr<Scene> scene);
+    // Работа с установкой exe-каталога для сцен, если нужно чего грузить
     void setExeDir(const std::string& exedir);
     std::string getExeDir() const;
-    void loadDefaultFont(const std::string& filename);
-    std::shared_ptr<sf::Font> getDefaultFont() const;
+    // Работа с курсором - курсор по умолчанию и добавить допкурсоры с числовым ключом
     void loadDefaultCursor(const std::string& filename);
     void addCursor(int code, const std::string& filename);
+    // Сделать активным курсор с номером
     void setCursor(int code);
-    void Run(std::shared_ptr<Scene> scene);
+    // Переключение к новой корневой сцене
     void SwitchToScene(std::shared_ptr<Scene> scene);
+    // Добавить новую сцену поверх
     void AddOverScene(std::shared_ptr<Scene> scene);
+    // Настройка окна - заголовок и иконка
     void setCaption(const std::string & str);
     void setIcon(const std::string& filename);
+    // Общие ресурсы движка для всех сцен - шрифт, строки, цвета
+    void loadDefaultFont(const std::string& filename);
+    std::shared_ptr<sf::Font> getDefaultFont() const;
     void loadTexts(const std::string& filename);
     const Texts& getTexts() const;
     void loadColors(const std::string& filename);
     const Colors& getColors() const;
+    // Преобразование из координат окна в мир по представлению
     sf::Vector2f getWorldPosByView(const sf::View& view, sf::Vector2i pos);
+    // Команда на закрытие игры
     void doClose();
+    // Команда на выход из текущей сцены (если сцена корневая - то будет выход из игры)
     void doExitScene();
 };
 
--- libs/SfmlGameEngine/include/SfmlGameEngine/Scene.h
+++ libs/SfmlGameEngine/include/SfmlGameEngine/Scene.h
@@ -17,15 +17,17 @@ private:
     std::vector<std::unique_ptr<sf::Texture>> textures;
     std::vector<std::unique_ptr<sf::SoundBuffer>> sounds;
 protected:
+    // Обращение к движку
     Engine* getEngine() const;
+    const Texts& getTexts() const;
+    const Colors& getColors() const;
+    // Загрузка ресурсов игры с автоудалением потом их текстур и буферов
     std::unique_ptr<sf::Sprite> loadSprite(const std::string& filename);
     std::unique_ptr<sf::Sound> loadSound(const std::string& filename);
     std::unique_ptr<sf::Text> loadText(int size);
     std::unique_ptr<sf::Text> loadText(const std::string & str, int size);
     std::unique_ptr<sf::Text> loadText(int size, sf::Color color);
     std::unique_ptr<sf::Text> loadText(const std::string& str, int size, sf::Color color);
-    const Texts& getTexts() const;
-    const Colors& getColors() const;
 public:
     virtual void Render(sf::RenderTarget& rendertarget);
     virtual void Update(float dt, const sf::Vector2i& mousePos, const std::vector<sf::Event>& events);
