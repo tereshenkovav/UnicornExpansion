5997d94d72dd71d6fdea0834c7566821a924a949
TAV <tereshenkov@mail.ru>
2026-04-17 10:32:56 +0300

    Добавлен вывод список благодарностей.

--- UnicornExpansion/UnicornExpansion/SceneHelp.cpp
+++ UnicornExpansion/UnicornExpansion/SceneHelp.cpp
@@ -1,6 +1,7 @@
 #include "SceneHelp.h"
 #include "SfmlGameEngine/Engine.h"
 #include "SceneStartMenu.h"
+#include <fstream>
 
 SceneHelp::SceneHelp(HelpType helptype) {
     this->helptype = helptype;
@@ -37,7 +38,29 @@ void SceneHelp::Init() {
     text_caption = loadText(getTexts().getStr(helptype == HelpType::Help?"Text_HelpCaption":"Text_About"), 24, sf::Color::White);
     text_caption->setPosition({ 512 - text_caption->getGlobalBounds().size.x / 2, 220 });
     
-    text_help = loadText(getTexts().getStr(helptype == HelpType::Help?"Text_FullHelp":"Text_FullAbout"), 20, sf::Color::White);
+    std::string str = "";
+    if (helptype == HelpType::Help) {
+        str = getTexts().getStr("Text_FullHelp");
+    }
+    else {
+        str = getTexts().getStr("Text_FullAbout");
+        if (std::filesystem::exists("credits.txt")) {
+            std::string line;
+            std::ifstream fin("credits.txt");
+            if (fin.is_open()) {
+                str += "\n\n" + getTexts().getStr("Text_Credits") + "\n\n";
+                std::vector<std::string> lines;
+                while (std::getline(fin, line))
+                    lines.push_back(line);
+                for (int i = 0; i < lines.size(); i++) {
+                    str += lines[i];
+                    if (i < lines.size() - 1) str += ((i + 1) % 4 == 0) ? "\n" : ", ";
+                }
+                fin.close();
+            }
+        }
+    }
+    text_help = loadText(str, 20, sf::Color::White);
     text_help->setPosition({ 160, 280});
 
     butok = std::make_unique<sfge::Button>(*getEngine()->getDefaultFont(), "OK", 18,
